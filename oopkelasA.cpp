77d729e5c323ed58768349fe8a311a72421dc4b6
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-04-12 14:36:08 +0700

    void inputdata

--- oopkelasA.cpp
+++ oopkelasA.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 class Mahasiswa {
-
+private:
+	int nim;
+	string nama;
+public:
+	void inputdata() {
+		cout << "Masukkan NIM : ";
+		cin >> nim;
+		cout << "Masukkan Nama ; ";
+		cin >> nama;
+	}
 };
