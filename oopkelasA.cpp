35f5536a03ae5ebcf6ad4f13397a1299bf5ea35b
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-04-12 14:53:16 +0700

    void Matakuliah: :inputMK

--- oopkelasA.cpp
+++ oopkelasA.cpp
@@ -30,6 +30,15 @@ public:
 
 };
 
+void Matakuliah::InputMK() {
+	cout << "\nMasukkan Kode Mata Kuliah : ";
+	cin >> kode;
+	cout << "Masukkan Nama Mata Kuliah : ";
+	cin >> namamk;
+	cout << "Masukkan Sks Mata Kuliah : ";
+	cin >> sks;
+}
+
 
 
 
